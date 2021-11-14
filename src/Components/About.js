468a808e120cc5cc55e4ae205a7d08b1f4070812
Sugyanee Sahu <sug.jan11@gmail.com>
2021-11-14 23:52:43 +0530

    todoslist

---
+++ src/Components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      this is my about comp
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dicta
+        tempore maiores culpa cum facilis, eaque magni tempora dolores
+        repudiandae perferendis voluptatem at sapiente? Nobis molestiae
+        perspiciatis ipsa esse assumenda aliquam sapiente cumque magnam!
+      </p>
+    </div>
+  );
+};
